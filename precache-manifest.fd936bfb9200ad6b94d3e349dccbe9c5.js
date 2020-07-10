50dd0367580902a965b91c019e7810c801be3a10
Yubo Diwu <yubodiwu@MacBook-Pro-3.local>
2020-07-10 11:09:04 -0700

    Updates

---
+++ precache-manifest.fd936bfb9200ad6b94d3e349dccbe9c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ade7db0c7154cc63d2bc173a4f1d49e",
+    "url": "/gp-mandarin-player/index.html"
+  },
+  {
+    "revision": "777ddabb8cadec12db1b",
+    "url": "/gp-mandarin-player/static/css/2.22fc193c.chunk.css"
+  },
+  {
+    "revision": "0a49c5ada0876bce6b02",
+    "url": "/gp-mandarin-player/static/css/main.d0ee7a36.chunk.css"
+  },
+  {
+    "revision": "777ddabb8cadec12db1b",
+    "url": "/gp-mandarin-player/static/js/2.3f291ac9.chunk.js"
+  },
+  {
+    "revision": "a17dd6d57e33dda6b1c2927e8d6cf97e",
+    "url": "/gp-mandarin-player/static/js/2.3f291ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a49c5ada0876bce6b02",
+    "url": "/gp-mandarin-player/static/js/main.d351c392.chunk.js"
+  },
+  {
+    "revision": "27af1475b469afde0ff2",
+    "url": "/gp-mandarin-player/static/js/runtime-main.8414a2b3.js"
+  }
+]);
